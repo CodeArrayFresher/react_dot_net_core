7ff8c42b2a930f4f77b5c9cc32d5adb319116461
Shubham Jadhav <Shubham Jadhav@DESKTOP-S878HSA>
2022-08-18 16:08:53 +0530

    Add project files.

---
+++ SimpleAuthSystem/DataAccessLayer/AuthDL.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using SimpleAuthSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAuthSystem.DataAccessLayer
+{
+    public class AuthDL : IAuthDL
+    {
+        public readonly IConfiguration _configuration;
+        public readonly SqlConnection _mySqlConnection;
+
+        public AuthDL(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            //_mySqlConnection = new MySqlConnection(_configuration["ConnectionStrings:MySqlDBConnectionString"]);
+
+            _mySqlConnection = new SqlConnection(_configuration.GetConnectionString("MySqlDBConnectionString"));
+
+            //string winAuth = this.Configuration.GetConnectionString("WinAuth");
+
+        }
+
+        public async Task<SignInResponse> SignIn(SignInRequest request)
+        {
+            SignInResponse response = new SignInResponse();
+            response.IsSuccess = true;
+            response.Message = "Successful";
+            try
+            {
+
+                if(_mySqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    await _mySqlConnection.OpenAsync();
+                }
+
+                string SqlQuery = @"SELECT * 
+                                    FROM [APITest].[dbo].[AuthTbl] 
+                                    WHERE UserName=@UserName AND [PassWord]=@PassWord;";
+
+                using (SqlCommand sqlCommand = new SqlCommand(SqlQuery, _mySqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.Text;
+                    sqlCommand.CommandTimeout = 180;
+                    sqlCommand.Parameters.AddWithValue("@UserName", request.UserName);
+                    sqlCommand.Parameters.AddWithValue("@PassWord", request.Password);
+                   // sqlCommand.Parameters.AddWithValue("@Role", request.Role);
+                    using (DbDataReader dataReader = await sqlCommand.ExecuteReaderAsync())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            response.Message = "Login Successfully";
+                        }
+                        else
+                        {
+                            response.IsSuccess = false;
+                            response.Message = "Login Unsuccessfully";
+                            return response;
+                        }
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+
+            }
+
+            return response;
+        }
+
+        public async Task<SignUpResponse> SignUp(SignUpRequest request)
+        {
+            SignUpResponse response = new SignUpResponse();
+            response.IsSuccess = true;
+            response.Message = "Successful";
+            try
+            {
+                if (_mySqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    await _mySqlConnection.OpenAsync();
+                }
+
+                //if (!request.Password.Equals(request.ConfigPassword))
+                //{
+                //    response.IsSuccess = false;
+                //    response.Message = "Password & Confirm Password not Match";
+                //    //return response;
+                //}
+
+                string SqlQuery = @"INSERT INTO  [APITest].[dbo].[AuthTbl]
+                                    (UserName, [PassWord]) VALUES 
+                                    (@UserName, @PassWord)";
+
+                using (SqlCommand sqlCommand = new SqlCommand(SqlQuery, _mySqlConnection))
+                {
+                    sqlCommand.CommandType = System.Data.CommandType.Text;
+                    sqlCommand.CommandTimeout = 180;
+                    sqlCommand.Parameters.AddWithValue("@UserName", request.UserName);
+                    sqlCommand.Parameters.AddWithValue("@PassWord", request.Password);
+                    //sqlCommand.Parameters.AddWithValue("@Role", request.Role);
+                    int Status = await sqlCommand.ExecuteNonQueryAsync();
+                    if(Status <= 0)
+                    {
+                        response.IsSuccess = false;
+                        response.Message = "Something Went Wrong";
+                        return response;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+
+            }
+
+            return response;
+        }
+    }
+}
